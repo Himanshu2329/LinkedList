402ec7b4d691dab52752d370e4af8c4d00cb2e9d
Himanshu2329 <kumarhimanshu4390@gmail.com>
2022-09-09 00:36:15 +0530

    Object Done

--- Java Oops/ObjectAndClass.java
+++ Java Oops/ObjectAndClass.java
@@ -1,24 +1,40 @@
 import java.util.*;
 
+/**
+ * ObjectAndClass
+ */
 public class ObjectAndClass {
-    public static class InnerObjectAndClass01 {
-        int age;
-        String name;
 
-        void sayhi() {
-            System.out.println(name + age + "says hi");
-        }
-
+    public void eat(){
+        System.out.println("kha rha h");
     }
-
+    public void run(){
+        System.out.println("bhag rha h");
+    }
     public static void main(String[] args) {
-        InnerObjectAndClass01 p1 = new InnerObjectAndClass01();
-        p1.age = 10;
-        p1.name = "a";
-        p1.sayhi();
-        InnerObjectAndClass01 p2 = new InnerObjectAndClass01();
-        p2.age = 101;
-        p2.name = "aa";
-        p2.sayhi();
+        System.out.println("1");
+        ObjectAndClass buzzo=new ObjectAndClass();
+        buzzo.eat();
+        buzzo.run();
+        Birds aa=new Birds();
+        aa.Fly();
+        animal bruno=new animal();
+        bruno.color="blue";
+        bruno.age=25;
+        // bruno.hello();
+        System.out.println(bruno.color+" "+bruno.age);
     }
+}
+class Birds{
+    public void Fly() {
+        System.out.println("udd rha h");
+    }
+}
+class animal{
+    String color;
+    int age;
+    
+    // void hello(){
+    //     System.out.println(color+age);
+    // }
 }
